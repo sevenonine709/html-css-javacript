ef166aba71e8345d8d4bae3f8183af572df8b938
Zoltan Cs <cserges@gmail.com>
2021-07-27 17:37:47 +0300

    Added list in for of a table created using DOM element

--- js/employees.js
+++ js/employees.js
@@ -3,6 +3,8 @@ const body=document.getElementsByTagName("BODY")[0];
 const infirstName=document.getElementById("firstName");
 const inlastName=document.getElementById("lastName");
 const selgendName=document.getElementById("gendName");
+const tabelName=document.getElementById("tableName")
+const indexfirst = 1;
 
 const special=/[*|\":<>[\]{}`\\()';@&$1234567890]/;
 infirstName.value=null;
@@ -18,7 +20,20 @@ function Person(first, last, gen, ftitle, vsalary) {
     this.salaryValue = vsalary; 
      }
   
+function addtable(objNames){
 
+        let tbody = document.getElementById("tbody");
+      
+        let row = document.createElement('tr');
+        let row_data_1 = document.createElement('td');
+        row_data_1.innerHTML = objNames.firstName;
+        let row_data_2 = document.createElement('td');
+        row_data_2.innerHTML = objNames.lastName;
+        row.appendChild(row_data_1);
+        row.appendChild(row_data_2);
+        tbody.appendChild(row);
+    
+}
   
 function clickItem (){
     
@@ -32,12 +47,14 @@ function clickItem (){
         else{
             const empData = new Person (infirstName.value, inlastName.value, selgendName.value);
             employeName.push(empData);
-            
+            console.log(empData);
             
             for (let valobj of employeName) { 
                 console.log(valobj.firstName);
             }
-        
+            addtable(empData);
+
+
             infirstName.value="";
             inlastName.value="";
         }
