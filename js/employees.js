93335c49d7de00973bef6b62e0176232f6c19e52
Zoltan Cs <cserges@gmail.com>
2021-07-27 22:08:52 +0300

    JS homework 1 plus bonus

--- js/employees.js
+++ js/employees.js
@@ -6,6 +6,22 @@ const selgendName=document.getElementById("gendName");
 const tabelName=document.getElementById("tableName")
 const indexfirst = 1;
 
+const homew1=document.getElementById("homew1");
+const homew2=document.getElementById("homew2");
+const homew3=document.getElementById("homew3");
+
+
+homew1.onclick = function() { 
+    document.getElementById("page1").style.display="block";
+    
+}
+homew2.onclick = function() { 
+    document.getElementById("page1").style.display="none";
+    
+   
+}
+
+
 const special=/[*|\":<>[\]{}`\\()';@&$1234567890]/;
 infirstName.value=null;
 inlastName.value=null;
@@ -23,26 +39,34 @@ function Person(first, last, gen, ftitle, vsalary) {
 function addtable(objNames){
 
         let tbody = document.getElementById("tbody");
-<<<<<<< HEAD
-      
-=======
-     
->>>>>>> d3f7567b98065d30336f9d1a52cf3451e4e0c084
         let row = document.createElement('tr');
         let row_data_1 = document.createElement('td');
         row_data_1.innerHTML = objNames.firstName;
         let row_data_2 = document.createElement('td');
-<<<<<<< HEAD
         row_data_2.innerHTML = objNames.lastName;
-=======
-        row_data_2.innerHTML = objNaes.lastName;
->>>>>>> d3f7567b98065d30336f9d1a52cf3451e4e0c084
+       
+        let row_data_3 = document.createElement('td');
+            let btn = document.createElement('input');
+            btn.type = "button";
+            btn.className = "w3-button w3-ripple w3-red w3-right";
+            btn.value="Delete";
+            btn.onclick = function() {
+            this.closest('tr').style.display="none";
+            }
+        row_data_3.appendChild(btn);
         row.appendChild(row_data_1);
         row.appendChild(row_data_2);
+        row.appendChild(row_data_3);
         tbody.appendChild(row);
     
 }
   
+function listNames(){
+    for (let valobj of employeName) { 
+        addtable(valobj);
+    }
+}
+
 function clickItem (){
     
     if (infirstName.value==="" || inlastName.value==="") {
